3fee16a3dc0f1b8905834a850e2b3bc1128a608e
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-13 22:28:39 +0000

    task4 updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-/*This function outputs what programming is */
+/**This function outputs what programming is */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle
+			\n");
 	return (0);
 }
