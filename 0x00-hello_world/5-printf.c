0b50fc19c2731de0d2ad6de604d42735951dc3b5
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-13 22:55:12 +0000

    task5-Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - this function demonstrates the usage of printf
+ *
+ *Description: the function prints a statement to the console
+ *Return: 0 after successful execution of the program
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
