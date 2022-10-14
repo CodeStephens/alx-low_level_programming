82a673f201a7989ee04c60fdeda1ac2a21265726
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-14 23:02:36 +0000

    updated

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,11 +21,11 @@ int main(void)
 	}
 	else if (lastDigit == 0)
 	{
-		printf("Last digit of %d is %d and not 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 	}
 	return (0);
 }
