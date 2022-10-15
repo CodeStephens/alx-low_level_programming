b63b897eff674e0fa9c847f606a2d7aa8826d437
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-15 00:16:34 +0000

    updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,7 +17,7 @@ int main(void)
 		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
 			putchar(num1 + '0');
-			putchar((num2 + '0');
+			putchar(num2 + '0');
 
 			if (num1 == 8 && num2 == 9)
 			continue;
