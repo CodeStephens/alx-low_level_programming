ecbd9ab1670c5fe1c2fb01946de34b07dde6472f
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-15 00:03:32 +0000

    updated

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-  
-/** 
- * main - prints out all numbers in base 16 in lowercase 
- * 
+
+/**
+ * main - prints out all numbers in base 16 in lowercase
+ *
  * Description: putchar() is strictly used for ouputting the numbers
  * Return: 0 is ouputted for successful code execution
- */ 
+ */
 int main(void)
 {
-    int num;
-    char ch;
-    for (num = 0; num <= 9; num++)
-        putchar(num+'0');
-    for (ch = 'a'; ch <= 'f'; ch++)
-        putchar(ch);
-    putchar('\n');
-    return (0);
+	int num;
+	char ch;
+
+	for (num = 0; num <= 9; num++)
+		putchar(num + '0');
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
 }
