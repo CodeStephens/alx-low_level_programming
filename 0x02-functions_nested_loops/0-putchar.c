b2abf6e04b91270b7bee5ba170fb27c230aea09f
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-18 14:50:30 +0000

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+ *main - Entry point
+ *
+ * Description: prints the word _putchar to the console
+ * Return: 0 for successful code execution
+ */
+int main(void)
+{
+	_putchar("_putchar");
+	_putchar("\n");
+	return (0);
+}
