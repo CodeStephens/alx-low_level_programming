ed7e16395d86c0df50adcb03dd63c9a62abb00f2
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-18 22:06:11 +0000

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * abs - computes the absolute of any given number
+ * @parameter: integer declared parameter to be checked
+ *
+ * Description: the absolute value of the argument is printed to the console
+ * Return: 0 or any integer value greater than 0 for successful code execution
+ */
+int _abs(int)
+{
+	int c; /** c is the parameter for the function signature*/
+
+	if (c >= 0)
+		return (c);
+	else
+		return (c * -1); 
+}
