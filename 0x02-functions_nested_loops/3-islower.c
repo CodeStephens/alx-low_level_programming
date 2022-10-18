7d536d84e0e21a947302cc1148fc07c3d0ed27ea
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-18 20:45:39 +0000

    updated

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 
 /**
  *_islower - checks for lowercase validity of inputted character
- *@ c: integer parameter declared with function
+ *@c: integer parameter declared with function
  *
  * Description: if conditional statement is use for the logical test on
  * c declared as integer
  * Return: 1 if 'c' is lowercase, else 0
  */
-int _islower(int c) /*@c: integer parameter declared*/
+int _islower(int c) 
 {
 	if (c >= 97 && c <= 122)
 	{
