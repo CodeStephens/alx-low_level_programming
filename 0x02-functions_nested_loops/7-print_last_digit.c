66e7a87e434296a2006357a98af6821f597c7483
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-18 22:42:16 +0000

    updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,12 @@
 int print_last_digit(int n)
 {
 	int lastDigit;
-       
+
 	if (n >= 0)
 		lastDigit = n % 10;
 	else
 		lastDigit = (n * -1) % 10;
 	_putchar(lastDigit + '0');
+	_putchar('\n');
 	return (lastDigit);
 }
