9e436deabf22597fe8e27008ec4046ab621f8bd1
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-19 12:52:49 +0000

    updated

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int count = 0;
 
-	long int initializingNum = 1;
-	long int initializingNum2 = 2;
-	long int nextNum;
+	long long int initializingNum = 1;
+	long long int initializingNum2 = 2;
+	long long int nextNum;
 
 	while (count <= 98)
 	{
