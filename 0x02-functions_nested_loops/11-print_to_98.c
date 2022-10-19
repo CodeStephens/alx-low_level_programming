0f35e49661e7fca34ad80e211d37fc15270ab350
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-19 01:38:57 +0000

    task11-print_to_98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - writes the array of natural numbers in a formatted pattern
+ * @n: given parameter during function call
+ *
+ * Description: the printed numbers are separated by ',' and ' ' to the console
+ * Return: 0 for successful code execution
+ */
+int print_to_98(int n)
+{
+	int i;
+	
+	for (i = n; i <= 98; i++)
+	{
+		if (i < 98)
+		{
+			printf("%d, ", i);
+		}
+		else
+			printf("%d\n", i);
+	}
+}
