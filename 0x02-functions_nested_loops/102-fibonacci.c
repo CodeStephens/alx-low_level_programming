320b1d32acd5ffa3ed2903e5e11d88f4ee63dffc
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-19 03:11:23 +0000

    updated

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -17,13 +17,13 @@ int main(void)
 	while (count < 50)
 	{
 		if (count == 0)
-			printf("%d", initializingNum);
+			printf("%ld", initializingNum);
 		else if (count == 1)
-			printf(", %d", initializingNum2);
+			printf(", %ld", initializingNum2);
 		else
 		{
 			nextNum = initializingNum + initializingNum2;
-			printf(", %d", nextNum);
+			printf(", %ld", nextNum);
 			initializingNum =  initializingNum2;
 			initializingNum2 = nextNum;
 		}
