9022784683d413349b441e7c65c4b0dd12e3aeb3
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-19 03:16:49 +0000

    task 14-fibonnaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+  * main - program entry point
+  *
+  * Description: computes the first 50 fibonacci numbers
+  * Return: nothing due to the void nature of the function datatype
+  */
+int main(void)
+{
+
+	long int initializingNum = 1;
+	long int initializingNum2 = 2;
+	long int nextNum;
+
+	while (initializingNum2 < "4,000,000")
+	{
+		if (count == 0)
+			printf("%ld", initializingNum);
+		else if (count == 1)
+			printf(", %ld", initializingNum2);
+		else
+		{
+			nextNum = initializingNum + initializingNum2;
+			printf(", %ld", nextNum);
+			initializingNum =  initializingNum2;
+			initializingNum2 = nextNum;
+		}
+	}
+
+	printf("\n");
+	return (0);
+}
