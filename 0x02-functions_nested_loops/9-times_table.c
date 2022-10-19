f2f31978814e0c12697b194785c3ee8117df4787
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-19 00:05:22 +0000

    updated

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * times_tables - multiplication tables from 0 to 9
+ * times_table - multiplication tables from 0 to 9
  *
  * Description: prints the output of multiplication table from 0 to 9
  * Return: 0 for successful code execution
@@ -18,15 +18,16 @@ void times_table(void)
 			if (multResult < 10)
 			{
 				_putchar(multResult + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 			}
 			else
 			{
 				_putchar((multResult / 10) + '0');
 				_putchar((multResult % 10) + '0');
-			}
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
+				_putchar(',');
+				_putchar(' ');
 		}
 		_putchar('\n');
 	}
