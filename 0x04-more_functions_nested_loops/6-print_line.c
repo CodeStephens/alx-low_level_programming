dd579d4f61f061b153e9351ca525b8dd918d0fab
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-21 01:20:08 +0000

    updated

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
  * print_line - print '-' to the standard output, followed by a new line
- * @n:input function parameter, which determines the number of '_' to be printed
+ * @n:input function parameter,which determines the number of '_' to be printed
  *
  * Description: _putchar function is used for this purpose
  * Return: nothing because of the "void" nature of the function datatype
