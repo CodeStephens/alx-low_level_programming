04c20eb905490575ea39380e0d1e288b7f058e67
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-21 02:49:38 +0000

    updated

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,7 +2,8 @@
 
 /**
  * print_triangle - use '#' to print triangle to the standard output
- * @size:input function parameter, which determines the number of '#' to be printed
+ * @size:input function parameter, which determines the number of '#' to
+ * be printed
  *
  * Description: _putchar function is used for this purpose
  * Return: nothing because of the "void" nature of the function datatype
