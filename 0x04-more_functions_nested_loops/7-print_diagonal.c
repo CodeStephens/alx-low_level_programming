223537703aec60825256464f8b3e47a64d8945ad
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-21 01:21:21 +0000

    updated

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,7 +2,7 @@
 
 /**
  * print_diagonal - print '\' to the standard output, followed by a new line
- * @n:input function parameter, which determines the number of '\' to be printed
+ * @n:input function parameter,which determines the number of '\' to be printed
  *
  * Description: _putchar function is used for this purpose
  * Return: nothing because of the "void" nature of the function datatype
