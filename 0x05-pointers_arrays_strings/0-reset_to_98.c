1ee7aff982611bd189690f172f0af6c0a3702652
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-24 13:34:00 +0000

    task0 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - the function resets the value of a parameter to 98
+ * @n: given function parameter
+ *
+ * Description: the technique of function is used
+ * Return: nothing!
+ */
+void reset_to_98(int *n)
+{
+	int *p = &n;
+
+	p = 98;
+}
