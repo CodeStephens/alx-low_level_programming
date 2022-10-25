fca7e70b3bf6e2ce481a95ccdffc112651175b6e
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-25 02:40:45 +0000

    updated

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,22 +3,19 @@
 /**
  * rev_string - prints a given string in reverse to the console
  * @s: given functional parameter (type string)
- * 
+ *
  * Description: a loop and _putchar is utilized to achieve this aim
  * Return: nothing!
  */
 void rev_string(char *s)
 {
 	int len = 0;
-	int i;
 
-	while (*s != 0)
-	{
+	while (*(s + len))
 		len++;
-		s++;
-	}
-	for (i = len; i != 0; i--)
+	len -= 1;
+	while (len >= 0)
 	{
-		_putchar(*(s+i));
+		_putchar(*(s + i));
 	}
 }
