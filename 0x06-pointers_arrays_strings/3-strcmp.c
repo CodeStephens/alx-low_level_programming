29d8015889d6003bf59ce8ef338ace5d66448b2d
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-28 04:08:57 +0000

    updated

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,16 +15,24 @@ int _strcmp(char *s1, char *s2)
 	while (*s1 != '\0' && *s2 != '\0')
 	{
 		if (*s1 == *s2)
+		{
 			result = 0;
+			s1++;
+			s2++;
+		}
 		else
 		{
 			if (*s1 > *s2)
+			{
 				result = 15;
+				break;
+			}
 			else
+			{
 				result = -15;
+				break;
+			}
 		}
-		s1++;
-		s2++;
 	}
 	return (result);
 }
