24597dfdd3e690392a26e585ba4df822165278a6
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-28 03:06:39 +0000

    updated

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,7 +6,7 @@
  * @dest: string to concatenate/append to
  * @n: masximum number of bytes to be concatenated from src
  *
- * Description:
+ * Description: concatenation using loops
  * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
