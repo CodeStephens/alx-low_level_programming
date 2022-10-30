b649833435e96ff0b9192ae1ef40f245489811b9
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-30 21:59:13 +0000

    updated

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,11 +10,11 @@
 void print_number(int n)
 {
 	if (n < 0)
-	{	
+	{
 		n *= -1;
 		_putchar('-');
 	}
-	if (n/10)
-		print_number(n/10);
+	if (n / 10)
+		print_number(n / 10);
 	putchar((n % 10) + '0');
 }
