74090f10e1eba6f17523fcdc58adf814146237e0
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-31 14:09:58 +0000

    task0-memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_memset - to set n bytes of the memory area pointed to by s by constant
+ * byte b
+ * @s: function parameter whose character is to replaced by b
+ * @b: given constant byte function parameter
+ * @n: the maximum number of character to replaced in s
+ *
+ * Description:
+ * Return: character
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *initial = s;
+	unsigned int i = 0;
+
+	while (*s != '\0' && i < n)
+	{
+		*s = b;
+		i++;
+		s++;
+	}
+	return (initial);
+}
