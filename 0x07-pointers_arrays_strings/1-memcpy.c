4f8a68af1f70835155ca90f93968aa1af10699d6
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-10-31 14:47:02 +0000

    task1-memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_memcpy - to copy n bytes of memory area from src to dest
+ * 
+ * @src: function parameter whose character whose n bytes is to be copied
+ * @dest: function parameter character to be copued to
+ * @n: the maximum number of bytes to be copied
+ *
+ * Description:
+ * Return: character
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *initial = dest;
+	unsigned int i = 0;
+
+	while (*dest != '\0')
+		dest++;
+	while (i < n)
+	{
+		*dest = *src;
+		i++;
+		src++;
+		dest++;
+	}
+	return (initial);
+}
