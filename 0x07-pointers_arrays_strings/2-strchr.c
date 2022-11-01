65323a21d3dce11d5831579db1d9c9bad6f49d43
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-01 12:31:13 +0000

    updated

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,12 @@ char *_strchr(char *s, char c)
 {
 	while (*s)
 	{
-		if (*s == c)
+		if (*s != c)
+			s++
+		else
 			return (s);
-		s++;
 	}
-	if (*s == '\0')
-		return (NULL);
-	return (s);
+	if (c == '\0')
+		return (s);
+	return (NULL);
 }
