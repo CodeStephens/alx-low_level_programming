a6c9dac784d259f425eaabb2579262ea5dbd3080
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-02 03:22:18 +0000

    updatedp

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,7 +11,7 @@
 void print_diagsums(int *a, int size)
 {
 	int i = 0;
-	int j = size - 1;
+	int j;
 	int sum1 = 0;
 	int sum2 = 0;
 
@@ -20,6 +20,7 @@ void print_diagsums(int *a, int size)
 		sum1 = sum1 + a[i];
 		i = i + size + 1;
 	}
+	j = size - 1;
 	while (j < (size * size - 1))
 	{
 		sum2 += a[j];
