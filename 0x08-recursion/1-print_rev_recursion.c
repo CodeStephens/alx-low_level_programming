ed580847815ca06f126c148221d57fc723cfc583
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-02 20:06:41 +0000

    task1-print_rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse using recursion
+ * @s: string to be printed
+ *
+ * Description:
+ * Return: nothing!
+ */
+void _prints_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+}
