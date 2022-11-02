c11d2140f3dee01af438d077f03d32c69e4c0cda
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-02 22:52:04 +0000

    updated

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,13 +11,13 @@ int is_prime_number(int n)
 {
 	if (n == 3)
 		return (1);
-	else if (n < 0)
-	       return (-1);
 	else if (n % 2 == 0)
 		return (0);
-	else (n / (n - 2) != 0)
+	else if (n / (n - 2) != 0)
 	{
 		is_prime_number(n - 2);
 		return (1);
 	}
+	else
+		return (0);
 }
