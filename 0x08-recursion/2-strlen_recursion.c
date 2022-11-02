6b38c89887ff3833ab9ff55aee261943c71d01ae
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-02 20:28:19 +0000

    task 2-strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * int _strlen_recursion - returns the length of a string
+ * @s: given string as a functional parameter
+ *
+ * Description:
+ * Return: integer
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		count = 0;
+	else if (*s != '\0')
+	{
+		_strlen_recursion(s + 1);
+		return (count += 1);
+	}
+	else
+		return (1);
+}
