5fa6436a8c50b1b970f49b73b27c66105386a436
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-02 20:56:55 +0000

    updated

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,12 +4,12 @@
  * int _strlen_recursion - returns the length of a string
  * @s: given string as a functional parameter
  *
- * Description:
+ * Description: prints the length of a string
  * Return: integer
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	return _strlen_recursion((s + 1) + 1);
+	return (_strlen_recursion((s + 1) + 1));
 }
