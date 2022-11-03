8e2fb6d937fae03cb36a97bf733cbc7f66df6cbc
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-03 08:01:42 +0000

    updated

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -25,10 +25,10 @@ int is_palindrome(char *s)
 {
 	if (*s == '\0')
 		return (1);
-	else if (*s == *(s + p))
+	else if (*s == *(s + *p))
 	{
 		is_palindrome(s + 1);
-		is_palindrome(s + p - 1);
+		is_palindrome(s + *p - 1);
 	}
 	else
 		return (0);
