073c4dc8fa7a45d5b6295240db41734acc0ff43b
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-03 05:21:52 +0000

    task100 - palindrome

---
+++ 0x08-recursion/100-is_palidrome.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks if a given string is a palindrome
+ * countString - an intermediary function to determine the string length
+ * @s: string pointer to a string
+ *
+ * Description:
+ * Return: integer
+ */
+int countString(char *s)
+{
+	int res;
+
+	if (*s != '\0')
+		res = countString(s + 1) + 1;
+	else
+		return (0);
+	return (res);
+}
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	else if (*s == *(s + res))
+	{
+		is_palindrome(s + 1);
+		is_palindrome(s + res - 1);
+	}
+	else
+		return (0);
+}
