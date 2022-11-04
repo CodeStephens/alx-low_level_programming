622b1442c25b47eda8c495eeba782cc11066562e
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-04 14:31:25 +0000

    task3 - argsMul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point to the program
+ * @argc: argument count
+ * @argv: array of argument variable
+ *
+ * Description: multiplies 2 given argument integers and prints the result
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+	int res;
+	
+	if (argc == 3)
+		res = argv[1] * argv[2];
+		printf("%d\n", res);
+		return (0);
+	else
+		printf("Error\n");
+		return (1);
+}
