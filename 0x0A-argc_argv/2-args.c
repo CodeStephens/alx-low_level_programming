5758d425040fdb6d1a46e83076d76ca8671d214e
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-04 14:25:11 +0000

    task2-print all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point to the program
+ * @argc: argument count
+ * @argv: array of argument variable
+ *
+ * Description:
+ * Return: integer
+ */
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
