e14dd23bf851e91f6403af6d561c79dbd4f4e0ec
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-04 15:58:25 +0000

    task4 - add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main - Entry point to the program
+ * @argc: argument count
+ * @argv: array of argument variable
+ *
+ * Description: adds positive numbers
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	if (argc == 1)
+		printf("0\n");
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (isdigit(argv[i]))
+			{
+				sum += atoi(argv[i]);
+				printf("%d\n", sum);
+				return (0);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	return (0);
+}
