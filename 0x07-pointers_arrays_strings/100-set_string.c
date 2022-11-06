a94978f03e6d67ed67b2ed92fe243d7f2f51b565
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-06 16:54:14 +0000

    updated

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,7 +2,7 @@
 
 /**
  * set_string - sets the value of a pointer to a character
- * @**s: pointer to pointer character whose value is to be set
+ * @s: pointer to pointer character whose value is to be set
  * @to: character that the pointer will be set to
  *
  * Description:
@@ -10,11 +10,5 @@
  */
 void set_string(char **s, char *to)
 {
-	s = &to;
-
-	while (**s != '\0')
-	{
-		**s = *to;
-		s++;
-	}
+	*s = to;
 }
