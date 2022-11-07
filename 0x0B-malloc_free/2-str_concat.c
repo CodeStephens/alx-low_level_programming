eaad7c8f52f97100fff66cad6d3fbf10dcf1e81d
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-07 23:13:08 +0000

    task2-str_concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _strlen - evaluates the length of a given string
+ * @n: character pointer to a string which the length is to be evaluated
+ *
+ * Description:
+ * Return: integer
+ */
+int _strlen(char *n)
+{
+	int count = 0;
+
+	if (*n != '\0')
+	{
+		count++;
+		return (count + _strlen(n + 1));
+	}
+	return (count);
+}
+/**
+ * *str_concat - concatenate two strings together
+ * @s1: first string
+ * @s2: second string
+ *
+ * Description:
+ * Return: a pointer to the newly concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	int len1;
+	int len2;
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	ptr = (char *) malloc((len1 + len2) * sizeof(char));
+	ptr = s1;
+	while (*s1)
+		s1++;
+	while (*s2)
+	{
+		*s1 = *s2;
+		s1++;
+		s2++;
+	}
+	return (ptr);
+}
