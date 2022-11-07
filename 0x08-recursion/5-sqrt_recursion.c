76a7772eccf391a6c8788faf67c508eb46487d9a
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-07 05:12:36 +0000

    task5-sqrt_recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * sqrt_check - evaluates the square root of a given number 
+ * @num: integer functional parameter in which square root is sort for
+ * @n: check to see if c is square root of the number
+ *
+ * Return: The square root value
+ */
+int sqrt_check(int num, int n)
+{
+	if (n * n == num)
+	{
+		return (n);
+	}
+	else if (n >= num / 2)
+		return (-1);
+	return (sqrt_check(num, n + 1));
+}
+
+/**
+ * _sqrt_recursion - evaluates the square root of given number
+ * @num: given number in which square root is sort for
+ *
+ * Return: The square root or -1 if there is no natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (num == 1)
+		return (1);
+	else if (num < 0)
+		return (-1);
+	else if (num == 0)
+		return (0);
+	return (sqrt_check(num, 1));
+}
