27d4ae90251116e12c7afc33d028d45ec0fee58c
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-07 04:51:52 +0000

    updated

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,28 +7,48 @@
  * Description:
  * Return: integer
  */
+int _strlen_recursion(char *s)
+{
+	int l;
+
+	l = 0;
+	if (*s != 0)
+	{
+		l++;
+		return (l + _strlen_recursion(s + 1));
+	}
+	return (l);
+}
+
+/**
+ * pal_check - Actual check for palindrome
+ * @s: String to check
+ * @l: Length of string
+ *
+ * Return: 1 if palindrome, 0 if otherwise
+ */
+int pal_check(char *s, int l)
+{
+	if (l <= 1)
+		return (1);
+	if (*s != s[l - 1])
+		return (0);
+	else
+		return (pal_check((s + 1), (l - 2)));
+}
+
+/**
+ * is_palindrome - Check if a string is a palindrome
+ * @s: String to check
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
 int is_palindrome(char *s)
 {
-	char *initial = s;
+	int l;
 
-	while (*s)
-	{
-		count++;
-		s++;
-	}
-	s = initial;
-	if (*s = '\0')
+	l = _strlen_recursion(s);
+	if (l <= 1)
 		return (1);
-	else
-	{
-		if (*s == *(s+(count - 1)))
-		{
-			s++;
-			is_palindrome(*(s+1));
-			return (1);
-		}
-		else
-			return (0);
-	}
-	return (0);
+	return (pal_check(s, l));
 }
