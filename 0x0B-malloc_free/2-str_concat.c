6448bfc3cdf2178b044fec81e104b7f11210c022
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-08 10:01:42 +0000

    updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -41,23 +41,26 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	if (s1 == NULL)
 	{
+		len1 = 0;
 		for (i = 0; i < len2; i++)
 			ptr[i] = s2[i];
 	}
-	else if (s2 == NULL)
+	if (s2 == NULL)
 	{
+		len2 = 0;
 		for (i = 0; i < len1; i++)
 			ptr[i] = s1[i];
 	}
-	else if (s1 == NULL && s2 == NULL)
-		ptr = NULL;
-	else
+	if (s1 == NULL && s2 == NULL)
 	{
-		for (i = 0; i < len1; i++)
-			ptr[i] = s1[i];
-		for (j = 0; j < len2; j++)
-			ptr[i + j] = s2[j];
+		len1 = 0;
+		len2 = 0;
+		return (ptr);
 	}
+	for (i = 0; i < len1; i++)
+		ptr[i] = s1[i];
+	for (j = 0; j < len2; j++)
+		ptr[i + j] = s2[j];
 	ptr[i + len2] = '\0';
 	return (ptr);
 }
