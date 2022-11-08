6bf34e1cc9e072f74432ba27f8a4ebaf1fda0bdf
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-08 01:50:57 +0000

    updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -34,24 +34,20 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	int j;
 
-	if (s1 == NULL)
-	{
-		return ('\0');
-		len1 = 0;
-	}
-	if (s2 == NULL)
-	{
-		return ('\0');
-		len2 = 0;
-	}
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	ptr = (char *) malloc((len1 + len2 + 1) * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < len1; i++)
+	if (s1 == NULL)
+		s1 = "";
+	else
+		for (i = 0; i < len1; i++)
 		ptr[i] = s1[i];
-	for (j = 0; j < len2; j++)
+	if (s2 == NULL)
+		s2 = "";
+	else
+		for (j = 0; j < len2; j++)
 		ptr[i + j] = s2[j];
 	ptr[i + len2] = '\0';
 	return (ptr);
