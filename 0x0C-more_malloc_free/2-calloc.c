bd586b43956e573104e3f01dba84ad7a0cfccabc
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-10 20:59:07 +0000

    updated

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,6 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
 	int *ptr;
 
 	if (nmemb == 0 || size == 0)
@@ -20,7 +19,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		free(ptr);
 		return (NULL);
 	}
-	for (i = 0; i < nmemb; i++)
-		ptr[i] = 0;
+	*ptr = 0;
 	return (ptr);
 }
