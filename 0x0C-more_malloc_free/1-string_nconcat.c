d4253a69dd1e45ba2533f48dda86765179942359
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-10 20:35:15 +0000

    tasks on malloc_more

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,77 @@
+#include "main.h"
+
+/**
+ * _strlen - evaluates the length of a string
+ * @str: given string to find the length
+ *
+ * Return: integer
+ */
+int _strlen(char *str)
+{
+	int i, len;
+
+	i = len = 0;
+	while (str[i])
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+/**
+ * *string_nconcat - concatenates two strings 
+ * @s1: first string
+ * @s2: second string 
+ * @n: n bytes of s2 to be concatenated to s1
+ *
+ * Return: character
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	int i, len1;
+	unsigned int j, len2;
+
+	i = 0;
+	if (s1 == NULL)
+		s1 = "\0";
+	if (s2 == NULL)
+		s2 = "\0";
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	ptr = malloc((len1 + n) * sizeof(char));
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	while (s1[i])
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while(s2[j])
+	{
+		if (n < len2)
+		{
+			while (j < n)
+			{
+				ptr[i] = s2[j];
+				i++;
+				j++;
+			}
+			return (ptr);
+		}
+		else if (n >= len2)
+		{
+			for (j = 0; j < len2; j++)
+			{
+				ptr[i] = s2[j];
+				i++;
+			}
+			return (ptr);
+		}
+	}	
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_calloc - allocates memory to an array
+ * @nmemb: size of array
+ * @size: byte size of each element of the array
+ *
+ * Return: character pointer to an array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		ptr[i] = 0;
+	return (ptr);
+}
