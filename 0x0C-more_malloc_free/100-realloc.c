f6ce4b0688bd28218721070dec92fd1f2921a72c
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-11 03:56:34 +0000

    task100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: previous size in bytes of the memory space for the pointer
+ * @new_size: new size in bytes of the new memory block
+ *
+ * Return: nothing!
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+	int i = 0;
+
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+	{
+		free(new_ptr);
+		exit(1);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	while (ptr[i])
+	{
+		new_ptr[i] = ptr [i];
+		i++;
+	}
+	
+
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,4 +1,4 @@
-#ifndef main_h
+fndef main_h
 #define main_h
 	
 	#include <stdio.h>
@@ -8,5 +8,6 @@
 	char *string_nconcat(char *s1, char *s2, unsigned int n);
 	void *_calloc(unsigned int nmemb, unsigned int size);
 	int *array_range(int min, int max);
+	void *_realloc(void *ptr, unsigned int old size,unsigned int new_size);
 
 #endif /* main_h */
