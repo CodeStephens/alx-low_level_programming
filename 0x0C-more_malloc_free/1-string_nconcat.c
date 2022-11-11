d936837c6c3da976d1b8f8476da3909e8905ae79
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-11 04:39:48 +0000

    updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,8 +29,7 @@ int _strlen(char *str)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	int i, len1;
-	unsigned int j, len2;
+	unsigned int i, j, len1, len2;
 
 	i = 0;
 	if (s1 == NULL)
