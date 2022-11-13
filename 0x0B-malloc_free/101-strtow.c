427a9967bf0f495fe83a37fcfea4031a3d0c5e60
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-13 16:17:17 +0000

    updated

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,111 @@
+#include "main.h"
+
+/**
+ * word_count - evaluates the numbers of words in the string
+ * @str: given string to check
+ *
+ * Description:
+ * Return: integer
+ */
+int word_count(char *str)
+{
+	int len, i;
+
+	i = len = 0;
+	while (str[i])
+	{
+		if (str[i] != ' ' && (str[i] == ' ' || str[i] == '\0'))
+		{
+			len++;
+			i++;
+		}
+		i++;
+	}
+	return (len);
+}
+/**
+ * word_size - evaluates the length of each word in the given string
+ * @str: pointer to the given string
+ * @words: number of words in the given string
+ *
+ * Return: integer
+ */
+int *word_size(char *str, int words)
+{
+	int i, word, count, *each_word_len;
+
+	each_word_len = malloc(words * sizeof(int));
+	if (each_word_len == NULL)
+		return (NULL);
+	i = word = 0;
+	for (word = 0; word < words; word++)
+	{
+		if (str[i] != ' ')
+		{
+			count = 0;
+			while (str[i] != ' ')
+			{
+				count++;
+				i++;
+			}
+			count++;
+			each_word_len[word] = count;
+		}
+		i++;
+	}
+	return (each_word_len);
+}
+/**
+ * **strtow -splits a string into words and return a pointer to the strings
+ * @str: pointer to the string to be split
+ *
+ * Description:
+ * Return: character
+ */
+char **strtow(char *str)
+{
+	char **ptr;
+	int i, j, k, word_counter, words, *each_word_len;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	words = word_count(str);
+	each_word_len = malloc(words * sizeof(int));
+	if (each_word_len == NULL)
+		return (NULL);
+	each_word_len = word_size(str, words);
+	ptr = malloc((words + 1) * sizeof(char *));
+	if (ptr == NULL)
+		return (NULL);
+	i = j = k = 0;
+	while (i < words && str[j] != '\0')
+	{
+		word_counter = i;
+		ptr[i] = malloc(each_word_len[i] + sizeof(char));
+		if (ptr[i] == NULL)
+		{
+			for (i = i - 1; i >= 0; i--)
+				free(ptr[i]);
+			free(ptr);
+			return (NULL);
+		}
+		while (str[j] != '\0' && i == word_counter)
+		{
+			if (str[j] == ' ')
+			{
+				while (str[j] != '\0' && str[j] != ' ')
+				{
+					ptr[i][k] = str[j];
+					j++, k++;
+				}
+				ptr[i][k] = '\0';
+				i++;
+				k = 0;
+			}
+			j++;
+		}
+	}
+	ptr[i] = NULL;
+	free(each_word_len);
+	return (ptr);
+}
