5c0668ea34c4f61153128feb4efbc4e2edb2cc79
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-13 19:08:14 +0000

    updated

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -14,7 +14,8 @@ int word_count(char *str)
 	i = len = 0;
 	while (str[i])
 	{
-		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'
+				|| str[i + 1] == '.'))
 			len++, i++;
 		i++;
 	}
