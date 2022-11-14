928f57c22a7e263fc58ee9fe4feebd0d65bb7444
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-14 21:58:24 +0000

    updated

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 
+/**
+ * main - program entry point
+ *
+ * Description: to print out program file name
+ * Return: 0
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
