cb415b894b75debf2bbadef53c75f46c39a140fc
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-15 03:23:31 +0000

    task1-init_dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - struct dog initialization
+ * @*d: userdefined datatype dog
+ * @name: second parameter
+ * @age: third parameter
+ * @owner: fourth parameter
+ *
+ * Return: nothing!
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog
+	{
+		struct dog *d;
+		char *name;
+		float age;
+		char *owner;
+	};
+}
