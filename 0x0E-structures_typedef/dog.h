6f991e76adbe62411651319c2dc88e4a87e4ed18
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-15 03:08:49 +0000

    updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef struct_dog
+#define struct_dog
+
 /**
  * struct dog - userdefined datatype dog
  * @name: first member
@@ -6,14 +9,11 @@
  *
  * Description:
  */
-#ifndef struct_dog
-#define struct_dog
-
-	struct dog
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
 #endif /* struct_dog */
