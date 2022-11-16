87cc64439933ce366927a7f65cc27bd00a891383
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-16 17:03:22 +0000

    task1-array_iterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,8 @@
 #ifndef main_h
 #define main_h
 
-	#include <stdo.h>
 `	int _putchar(int);
 	void print_name(char *name, void (*f)(char *));
+	void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* main_h */
