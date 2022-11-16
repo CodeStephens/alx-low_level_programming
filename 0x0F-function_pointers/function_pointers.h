f122e50355b647e2c6414a5eec4df5c959583519
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-16 17:14:12 +0000

    updated

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
 #ifndef main_h
 #define main_h
 
-`	int _putchar(int);
+	#include <stddef.h>
+	int _putchar(int);
 	void print_name(char *name, void (*f)(char *));
 	void array_iterator(int *array, size_t size, void (*action)(int));
 
