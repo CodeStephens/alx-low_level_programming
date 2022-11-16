995c3e7af39f895c17b0796f9be9e1f3e270ea26
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-16 18:07:59 +0000

    updated

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
+	if (array == NULL)
+		return (NULL);
+	if (action == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
