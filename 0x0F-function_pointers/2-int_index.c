534257e9aec3fd9ff697242ddbe1a421198b6e1a
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-16 18:06:39 +0000

    updated

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,6 +12,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int count;
 
+	if (array == NULL)
+		return (-1);
+	if (cmp == NULL)
+		return (-1);
 	if (size <= 0)
 		return (-1);
 	for (count = 0; count < size; count++)
