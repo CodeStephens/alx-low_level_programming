d84748e2efdf072d1a01d7bab8de3c6ef6b193c4
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-18 01:01:34 +0000

    updated

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,43 +1,48 @@
 #include "variadic_functions.h"
 
+/**
+ * print_all - print all items in the argument to the console
+ * @format: pointer to string format (c, i, f & s) to be printed
+ *
+ * Return: nothing!
+ */
 void print_all(const char * const format, ...)
 {
-	va_list valist;
-	int n = 0, i = 0;
-	char *sep = ", ";
+	va_list args;
+	int n = 0;
+	int len = 0;
+	char *delimiter = ", ";
 	char *str;
 
-	va_start(valist, format);
+	va_start(args, format);
 
-	while (format && format[i])
-		i++;
+	while (format && format[len])
+		len++;
 
 	while (format && format[n])
 	{
-		if (n  == (i - 1))
-		{
-			sep = "";
-		}
+		if (n  == (len - 1))
+			delimiter = "";
 		switch (format[n])
 		{
 		case 'c':
-			printf("%c%s", va_arg(valist, int), sep);
+			printf("%c%s", va_arg(args, int), delimiter);
 			break;
 		case 'i':
-			printf("%d%s", va_arg(valist, int), sep);
+			printf("%d%s", va_arg(args, int), delimiter);
 			break;
 		case 'f':
-			printf("%f%s", va_arg(valist, double), sep);
+			printf("%f%s", va_arg(args, double), delimiter);
 			break;
 		case 's':
-			str = va_arg(valist, char *);
+			str = va_arg(args, char *);
 			if (str == NULL)
 				str = "(nil)";
-			printf("%s%s", str, sep);
+			printf("%s%s", str, delimiter);
 			break;
 		}
 		n++;
 	}
 	printf("\n");
-	va_end(valist);
+	va_end(args);
 }
