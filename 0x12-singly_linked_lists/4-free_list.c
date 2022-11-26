1a0721cc72e6018b159e410e8e03eaa536f08e8a
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-26 15:46:18 +0000

    updated

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,15 +8,10 @@
  */
 void free_list(list_t *head)
 {
-	int count = 0;
-	int i;
-
 	if (head == NULL)
 		return;
 	if (head != NULL)
-	{
 		free_list(head->next);
-		free_list(head->str);
-	}
+	free(head->str);
 	free(head);
 }
