38e5fa3b3e482045be0cda285b8936183317c9d6
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-27 20:30:32 +0000

    updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,14 @@
  */
 size_t print_list(const list_t *h)
 {
-	int count;
+	int count = 0;
 
-	count = 0;
 	if (h == NULL)
 		return (0);
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
