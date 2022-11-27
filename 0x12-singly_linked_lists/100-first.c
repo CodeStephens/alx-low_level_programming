ac8a0ce65fc91724e596658dad1c7fd1c3f86aec
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-27 21:48:37 +0000

    updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@
  */
 void start_up_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon
-			my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon "
+		       "my back!\n");
 }
