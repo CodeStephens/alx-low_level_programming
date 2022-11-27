20b88984c049cd33b8e25fff2c317dae6c31f2de
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-27 21:20:35 +0000

    updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,8 +6,8 @@
  * Description: prints out a sentence prior to commencement of main
  * Return: nothing!
  */
-void start_up_before_main (void)
+void start_up_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my"
-			" back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon
+			my back!\n")
 }
