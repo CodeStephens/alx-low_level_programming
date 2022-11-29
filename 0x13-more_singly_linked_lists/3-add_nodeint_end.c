d349199fcf685c386703bf34ba62e7caca657d06
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-29 16:58:09 +0000

    updated

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -22,7 +22,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = new;
 		return (new);
 	}
-	temp = *head
+	temp = *head;
 	while (temp->next != NULL)
 		temp = temp->next;
 	temp->next = new;
