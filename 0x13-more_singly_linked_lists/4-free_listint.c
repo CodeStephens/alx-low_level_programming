6cf520e59f07ba77f439cd7804c14892fdd94fce
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-29 12:08:20 +0000

    task4-free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees the listint_t list
+ * @head: pointer to head of the listint_t lists
+ *
+ * Return: nothing!
+ */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		free(head->next);
+	}
+	free(head);
+}
