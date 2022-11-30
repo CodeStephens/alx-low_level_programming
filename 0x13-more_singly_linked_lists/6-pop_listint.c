61a5e3bca1afbc3d4af105b2e5ae147a524c2506
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-30 01:33:57 +0000

    updated

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,16 +9,12 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
-	listint_t *temp1;
 	int num;
 
 	if (head == NULL || *head == NULL)
 		return (0);
 	temp = *head;
-	temp = temp->next;
-	temp1 = temp->next;
 	num = temp->n;
 	free(temp);
-	(*head)->next = temp1;
 	return (num);
 }
