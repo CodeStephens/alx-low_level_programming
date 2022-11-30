4d53ea62bdbcb597ccadc5ccc4e54c19b8c81969
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-11-30 02:25:04 +0000

    task8-sum list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - gets the sum of all data(n) of the listint_t linked list
+ * @head: pointer to listint_t linked list
+ *
+ * Return: integer
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *temp;
+	int sum = 0;
+
+	temp = head;
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (0);
+}
