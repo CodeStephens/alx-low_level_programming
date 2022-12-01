ac3e6a273e2aa8a4c2ea517af83ba3d5051b0b5f
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-12-01 22:38:24 +0000

    task0 - binary conversion to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * expo - evaluates the a to the power of b
+ * @a: base integer
+ * @b: index integer
+ *
+ * Return: integer
+ */
+int expo(int a, int b)
+{
+	int i, result;
+
+	result = a;
+	for (i = 1; i < b; i++)
+	{
+		result *= a;
+	}
+	return (result);
+}
+/**
+ * binary_to_uint - converts a binary number to an unsigned int in decimal
+ * @b: pointer to a string of 1s and/or 0s
+ *
+ * Return: integer
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int len, i, sum;
+
+	if (b == NULL)
+		return (0);
+	while (b)
+	{
+		if (*b != 0 || *b != 1)
+			return (0);
+		len++;
+		b++;
+	}
+	for (i = 0; i < len; i++)
+	{
+		sum += expo(2, i);
+	}
+	return (sum);
+}
