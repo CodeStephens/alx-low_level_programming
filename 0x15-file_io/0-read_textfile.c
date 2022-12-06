ed4dde41d8788481e2f7904814baf96ad5c37e35
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-12-06 15:37:16 +0000

    task0-read textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * read_textfile: reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to be read from
+ * @letters: number of letters to be read and printed
+ *
+ * Return: integer
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f_open;
+	size_t count;
+
+	if (filename == NULL)
+		return (0);
+	f_open = open(filename, O_CREAT | O_WRONLY | O_APPEND, 600);
+	if (f_open < 0)
+		return (0);
+	count = write(f_open, filename, letters);
+	if (count != letters)
+		return (0);
+	close(f_open);
+	return (count);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef main_h
+#define main_h
+
+	#include <stdio.h>
+	#include <unistd.h>
+	#include <fcntl.h>
+	#include <stddef.h>
+	#include <stdlib.h>
+	int _putchar(int c);
+	ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* main_h */
