3f22f83f2d57407cb224345334215de62577f63a
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2022-12-08 19:02:42 +0000

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	f_open = open(filename, O_CREAT | O_RDWR, 0600);
+	f_open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (text_content == NULL)
 		return (1);
 	if (f_open < 0)
