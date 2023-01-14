f72ac7c1680bea6476fda6bc2f6b32e77af31420
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-01-14 08:52:39 +0000

    task0 added

---
+++ C/0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements and number of elements in a list
+ * @h: pointer to the head of the linked lists
+ *
+ * Description:
+ * Return: number of the elements in the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count;
+	const dlistint_t *temp;
+
+	if (h == NULL)
+		return (0);
+	temp = h;
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
