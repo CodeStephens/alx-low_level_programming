a4cead3eaadc7ff549df5cdd7dd3cf9c09ac8a27
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-01-14 12:14:29 +0000

    task4 added

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees the doubly linked list
+ * @head: pointer to the address of the head of the linked lists
+ *
+ * Description:
+ * Return: Nothing!
+ */
+void *free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+	
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
