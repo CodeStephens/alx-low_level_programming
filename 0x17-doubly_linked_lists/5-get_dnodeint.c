a3d0f0589b48c847dace11872be8e069a4ead12a
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-01-14 12:41:47 +0000

    task5 added

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - locates the node at a specified indes;
+ * @head: pointer to the address of the head of the linked lists
+ * @index: required index location of the node
+ *
+ * Description:
+ * Return: pointer to the node at the index
+ */
+dlistint *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp;
+	unsigned int count = 0;
+
+	while (count <= index)
+	{
+		if (count == index)
+			return (head);
+		head = head->next;
+		count++;
+	}
+	return (NULL)
+}
