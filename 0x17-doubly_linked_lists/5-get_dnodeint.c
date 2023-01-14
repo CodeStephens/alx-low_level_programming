d794ab50511d0bc71393c0bc24cfd5bce0a37cd7
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-01-14 12:52:26 +0000

    task5 modified

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,9 +12,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
 
-	if (head == NULL)
-		return (NULL);
-	while (count <= index)
+	while (count <= index && head != NULL)
 	{
 		if (count == index)
 			return (head);
