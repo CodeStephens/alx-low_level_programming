5254ab579362ddc0a0852cec8c70e499db29a5d7
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-01-14 14:09:17 +0000

    task7 modified

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,13 +22,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	newNode->n = n;
 	while (count <= idx)
 	{
 		temp = *h;
 		temp1 = (*h)->next;
-		if (count == idx && temp->next != NULL)
+		if ((count == idx) && (temp->next != NULL))
 		{
+			newNode->n = n;
 			newNode->prev = temp;
 			newNode->next = temp1;
 			temp->next = newNode;
@@ -37,7 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 		else if (count == idx && temp->next == NULL)
 			return (add_dnodeint_end(h, n));
-		*h = (*h)->next;
+		temp = temp->next;
 		count++;
 	}
 	return (NULL);
