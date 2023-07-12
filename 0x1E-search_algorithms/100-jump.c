1f331f2f7f05ad086919acf88f2002bb7504b7de
CodeStephens <oluwatosin.stephens@gmail.com>
2023-07-12 15:50:44 +0100

    task100 - jump_search algorithm added

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,45 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - searches for the first match of a given value and returns
+ * the index in a sorted array
+ * @array: pointer to the head of the aray to be searched
+ * @size: number of elements in the array
+ * @value: given value to be matched in the array
+ *
+ * Description: skips/jumps a block of array based on the square root of the
+ * size of the array, evaluates computational comparison, before linear search
+ * Return: integer if a match exist in the array, else -1
+ *
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t iter_step;
+	int prev, next, i;
+
+	//test for validity of the array
+	if (array == NULL)
+		return (-1);
+
+	//continue with the jump search algorithm
+	iter_step = sqrt(size);
+	prev = 0;
+	next = iter_step;
+
+	// Find the block where the target may reside
+	while (next < size && arr[next] < value)
+	{
+		prev = next;
+		next += iter_step;
+	}
+
+	// Perform linear search within the block
+	for (i = prev; i < next && i < n; i++)
+	{
+		if (arr[i] == target)
+			return i;
+	}
+	// Element not found
+	return (-1);
+}
