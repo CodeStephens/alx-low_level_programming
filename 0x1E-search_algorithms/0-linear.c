19712de11c1e1b22ec2e93ff6d8bc5dcf6b7229e
CodeStephens <oluwatosin.stephens@gmail.com>
2023-07-12 10:46:47 +0100

    updated linear_search task0 code

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -24,7 +24,7 @@ int linear_search(int *array, size_t size, int value)
 		if (array[i] != value)
 			continue;
 		else
-			return (i);
+			return (int(i));
 	}
 	return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,8 +4,8 @@
 /**
  * defining a user-defined header file
  */
-
-    #include <stdio.h>
+	
+	#include <stdio.h>
     int linear_search(int *array, size_t size, int value);
 
 
